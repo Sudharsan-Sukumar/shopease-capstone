c59b318b564a366100ee7477b2acf8daa332e355
Sudharsan <itzz.sudharsan@gmail.com>
2026-08-19 17:25:09 +0530

    Update environment.development.ts

--- Capstone-Ecommerce/Application.Client/src/environments/environment.development.ts
+++ Capstone-Ecommerce/Application.Client/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiBaseUrl: 'shopease-capstone--api-ezbeaxbjadcrcyfj.centralindia-01.azurewebsites.net',
+  apiBaseUrl: 'https://shopease-capstone--api-ezbeaxbjadcrcyfj.centralindia-01.azurewebsites.net/api',
 };
 
